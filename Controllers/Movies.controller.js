0ff66b07394e86ad886609b97dd2abe1a07275f0
muntherabdlrahman <munther.abdlrahman@gmail.com>
2021-07-01 18:15:57 +0300

    lab10 correctly work

--- Controllers/Movies.controller.js
+++ Controllers/Movies.controller.js
@@ -1,25 +1,58 @@
 const Movies = require("../Models/Movies.model");
 const PORT = process.env.PORT;
 const MOVIES_API_KEY=process.env.MOVIES_API_KEY;
+const Cache=require('../utils/cashe')
 const axios = require('axios');
 
 
 
+let cache1=new Cache();
+cache1['data']=[];
+
+
+
 const moviesController=('/movies', (req, res) => {
-    let movies;
+    let movies=[];
     let query = req.query.query
-  
-    let moviesUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIES_API_KEY}&query=${query}`
-    let moviesResp = axios.get(moviesUrl).then(response => {
-      movies = response.data.results;
-  
-      let callMovies = movies.map(el => {
-        return new Movies(el);
-      });
-  
-      res.json(callMovies);
+    let callMoviesArr=[];
+
+    if(query){
+      if(cache1.data.length>0){
+        callMoviesArr = cache1.data.map(ele => new Movies(ele));
+        console.log('==================== come from cache====================');
+        res.json(callMoviesArr);
+
+
+
+
+        
+
+      }else{
+        let moviesUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIES_API_KEY}&query=${query}`
+          axios.get(moviesUrl).then(response => {
+
+          movies = response.data.results;
       
-    }).catch(error=>res.send({message:error.message}));
+          callMoviesArr = movies.map(ele => new Movies(ele));
+
+          cache1['data']=movies;
+
+          console.log('==================== come from API ITSELF====================')
+      
+          res.json(callMoviesArr);
+          
+        }).catch(error=>res.send({message:error.message}));
+
+      }
+
+
+    }else{
+      res.send('Something wrong with lat & lon try again')
+    }
+
+
+
+  
   });
 
   module.exports=moviesController
---
+++ utils/cashe.js
@@ -0,0 +1,6 @@
+'use strict';
+
+class Cache{
+
+}
+module.exports=Cache
